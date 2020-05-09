3fc66081ab47c71b73ad4b0d3e99b9b1177152f8
Your Name <you@example.com>
2020-05-09 18:48:20 +0300

    f**k_this_life_trying_to_multiple_access_to_db

--- WCFServiceWebRole1/Properties/AssemblyInfo.cs
+++ WCFServiceWebRole1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BortoWeb")]
+[assembly: AssemblyTitle("WCFServiceWebRole1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BortoWeb")]
+[assembly: AssemblyProduct("WCFServiceWebRole1")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e856f1c4-f4fa-4675-9ed8-f2aa7aacfa06")]
+[assembly: Guid("1d9ba230-2f8a-4069-b86e-ede839581d95")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
